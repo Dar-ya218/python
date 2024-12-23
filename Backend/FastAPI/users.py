2b8610440b68518bce21fcfbf8e631653f333f41
Dasha <dasharoura13465@gmail.com>
2024-12-23 10:59:56 +0100

    Add endpoints to get all users and a user by ID

--- Backend/FastAPI/users.py
+++ Backend/FastAPI/users.py
@@ -7,11 +7,19 @@ app = FastAPI()
 
 #Entidad user
 class User(BaseModel):
+    id: int
     name: str
     surname: str
     url: str
     age: int
 
+users_list = [
+    User( id=1, name="Dasha", surname="Boch", url="https://www.linkedin.com/in/dasha-boch/", age=25),
+    User( id=2, name="Katya", surname="Lin", url="https://www.linkedin.com/in/katya-lin/", age=55),
+    User( id=3, name="Misty", surname="Pep", url="https://www.linkedin.com/in/misty-pep/", age=35)
+]
+   
+   
 @app.get("/usersjson")
 async def usersjson():
     return [{"name": "Dasha", "surname": "Boch", "url": "https://www.linkedin.com/in/dasha-boch/", "age": 25},
@@ -19,6 +27,11 @@ async def usersjson():
             {"name": "Misty", "surname": "Pep", "url": "https://www.linkedin.com/in/misty-pep/", "age": 35}]
 
 
-@app.get("/usersclass")
-async def usersclass():
-    return User(name="Dasha", surname="Boch", url="https://www.linkedin.com/in/dasha-boch/", age=25)
+@app.get("/users")
+async def users():
+    return users_list
+
+@app.get("/user/{id}")
+async def user(id: int):
+    user = filter(lambda user: user.id == id, users_list)
+    return list(user)[0]
