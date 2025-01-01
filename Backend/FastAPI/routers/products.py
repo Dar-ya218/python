381fa46a774af05f8a2996ff6c1edf75772b9d3c
Dasha <dasharoura13465@gmail.com>
2025-01-01 19:29:21 +0100

    add products router with endpoints to get all products and a product by ID

--- Backend/FastAPI/routers/products.py
+++ Backend/FastAPI/routers/products.py
@@ -1,7 +1,20 @@
 from fastapi import APIRouter
 
-router = APIRouter()
+router = APIRouter(prefix="/products",
+                   tags=["products"],
+                   responses={404: {"description": "No encontrado"}})
 
-@router.get("/products")
+
+products_list = ["Product 1",
+                "Product 2", 
+                "Product 3", 
+                "Product 4"]
+
+@router.get("/")
 async def get_products():
-    return ["Product 1", "Product 2", "Product 3", "Product 4"]
+    return products_list
+
+
+@router.get("/{id}")
+async def get_products(id: int):
+    return products_list[id]
