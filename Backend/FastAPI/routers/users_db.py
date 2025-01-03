fc87c12fe043f8fd260d22bf0b4ac4e084a6f9a7
Dasha <dasharoura13465@gmail.com>
2025-01-03 15:24:34 +0100

    add user

--- Backend/FastAPI/routers/users_db.py
+++ Backend/FastAPI/routers/users_db.py
@@ -1,24 +1,13 @@
-from fastapi import APIRouter, HTTPException
-from pydantic import BaseModel
+from fastapi import APIRouter, HTTPException, status
+from db.models.user import User
 
 router = APIRouter(prefix="/usersdb",
                    tags=["usersdb"],
-                   responses={404: {"description": "No encontrado"}})
+                   responses={status.HTTP_404_NOT_FOUND: {"description": "No encontrado"}})
 
 # Inicia el server:  uvicorn users_db:app --reload
 
-class User(BaseModel):
-    id: int
-    name: str
-    surname: str
-    url: str
-    age: int
-
-users_list = [
-    User( id=1, name="Dasha", surname="Boch", url="https://www.linkedin.com/in/dasha-boch/", age=25),
-    User( id=2, name="Katya", surname="Lin", url="https://www.linkedin.com/in/katya-lin/", age=55),
-    User( id=3, name="Misty", surname="Pep", url="https://www.linkedin.com/in/misty-pep/", age=35)
-]
+users_list = []
 
 @router.get("/")
 async def users():
@@ -37,12 +26,12 @@ async def user(id: int):
 async def user(id: int):
     return search_user(id)
 
-@router.post("/", status_code=201)
+@router.post("/", status_code=status.HTTP_201_CREATED)
 async def user(user: User):
     print(user)
     print(user.id)
     if type(search_user(user.id)) == User:
-        raise HTTPException(status_code=204, detail="El usuario ya existe")
+        raise HTTPException(status.HTTP_404_NOT_FOUND, detail="El usuario ya existe")
     else:
         users_list.append(user)
         return user
@@ -79,4 +68,4 @@ def search_user(id: int):
     try:
         return list(user)[0]
     except IndexError:
-        raise HTTPException(status_code=404, detail="Error user not found")
+        raise HTTPException(status.HTTP_404_NOT_FOUND, detail="Error user not found")
